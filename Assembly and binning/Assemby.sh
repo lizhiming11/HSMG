28a275df3aaece4729961600d671adf294a5e1a3
lizm <642998881@qq.com>
2023-07-10 15:45:40 +0800

    Update Assemby.sh

--- Assembly and binning/Assemby.sh
+++ Assembly and binning/Assemby.sh
@@ -6,4 +6,4 @@ binning outfile = $4
 
 metawrap assembly -1 $1 -2 $2  -o $3 -t 20 -m 2000
 
-metawrap binning --metabat2 --maxbin2 --concoct -t 20 -m 2000 --run-checkm -a $3/final_assembly.fasta -o $4 $1 $2
+metawrap binning --metabat2 -t 20 -m 2000 --run-checkm -a $3/final_assembly.fasta -o $4 $1 $2
