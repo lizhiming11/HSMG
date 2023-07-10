38155703551a5a3ea8bf3f8e9b527d8f321a4215
lizm <642998881@qq.com>
2023-07-10 15:57:39 +0800

    Update and rename Assemby.sh to run.sh

---
+++ UHSG Construction and Annotation/run.sh
@@ -0,0 +1,27 @@
+metaWRAP install : https://github.com/bxlab/metaWRAP
+fq1 = $1
+fq2 = $2
+assembly outfile = $3
+binning outfile = $4
+
+metawrap assembly -1 $1 -2 $2  -o $3 -t 20 -m 2000
+
+metawrap binning --metabat2 -t 20 -m 2000 --run-checkm -a $3/final_assembly.fasta -o $4 $1 $2
+
+Identifying 16s sequences：
+
+barrnap --quiet high_genomics/$1.fa -o high_genomics_16s/$1_16.fa
+
+gtdbtk annotation：
+gtdbtk identify --genome_dir genomics-dir --out_dir identify --extension fa --cpus 40
+gtdbtk align --identify_dir identify --out_dir align --cpus 40
+gtdbtk classify --genome_dir genomics-dir --align_dir align --out_dir classify -x fa --cpus 40 
+
+phylogenetic tree:
+iqtree2 -s gtdbtk.bac120.user_msa.fasta -m MFP
+
+card annotation:
+rgi main --input_sequence genome_dir/*.fa --output_file $1_card --input_type protein -a DIAMOND -n 20 --clean
+
+Functional annotation:
+python emapper.py -m diamond -i genome_dir/*.fa --output result -d bact --cpu 5
