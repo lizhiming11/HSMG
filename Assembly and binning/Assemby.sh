6081fa626e6b9d64b0617211e1b0a7de861107fc
lizm <642998881@qq.com>
2023-07-10 15:44:22 +0800

    Update and rename megahit.sh to Assemby.sh

---
+++ Assembly and binning/Assemby.sh
@@ -0,0 +1,9 @@
+metaWRAP install : https://github.com/bxlab/metaWRAP
+fq1 = $1
+fq2 = $2
+assembly outfile = $3
+binning outfile = $4
+
+metawrap assembly -1 $1 -2 $2  -o $3 -t 20 -m 2000
+
+metawrap binning --metabat2 --maxbin2 --concoct -t 20 -m 2000 --run-checkm -a $3/final_assembly.fasta -o $4 $1 $2
