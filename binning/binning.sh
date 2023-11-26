1aa02019b88c5c0583a7b6c84bf69ea9cfb1bf5b
lizm <642998881@qq.com>
2023-11-26 17:30:31 +0800

    Delete binning/binning.sh

--- binning/binning.sh
+++
@@ -1,3 +0,0 @@
-conda activate metawrap
-sample = $1
-metawrap binning --metabat2 --maxbin2 --concoct -t 20 -m 2000 --run-checkm -a 2.Assembly/$sample/final_assembly.fasta -o binning/$sample ./1.Cleandata/$sample_1.fastq ./1.Cleandata/$sample_2.fastq
