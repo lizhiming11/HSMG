097330c948bdd9b0d14b1f86f6b5ade00b238b34
lizm <642998881@qq.com>
2023-11-26 12:29:14 +0800

    Update clean.sh

--- Clean and rmhost data/clean.sh
+++ Clean and rmhost data/clean.sh
@@ -3,4 +3,4 @@ fq2 = $2
 outfile = $3
 sample = $4
 
-SOAPnuke filter -l 20 -q 0.5 -n 0.1 -d -m 5 -Q 2 -G -1 $fq1 -2 $fq2 -o $outfile -C $sample_clean.1.fq.gz -D $sample_clean.2.fq.gz
+fastp -i $fq1 -I $fq2 -u 45 -q 20 -u 5 -o $sample_clean.1.fq.gz -O $sample_clean.2.fq.gz
