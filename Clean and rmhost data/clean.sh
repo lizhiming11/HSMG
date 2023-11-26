0839ab6d9d32602aca9dd87e74c014fb6a9c6b9e
lizm <642998881@qq.com>
2023-11-26 17:17:19 +0800

    Update clean.sh

--- Clean and rmhost data/clean.sh
+++ Clean and rmhost data/clean.sh
@@ -1,6 +1,6 @@
 fq1 = $1
 fq2 = $2
 outfile = $3
-sample = $4
 
-fastp -i $fq1 -I $fq2 -u 45 -q 20 -u 5 -o $sample_clean.1.fq.gz -O $sample_clean.2.fq.gz
+
+fastp -i $fq1 -I $fq2 -u 45 -q 20 -u 5 -o $3sample_clean.1.fq.gz -O $3sample_clean.2.fq.gz
