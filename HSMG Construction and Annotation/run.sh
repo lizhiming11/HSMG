2bac411321edd25e717f8f40aebe316c78e974bd
lizm <642998881@qq.com>
2024-05-15 16:32:18 +0800

    Update run.sh

--- HSMG Construction and Annotation/run.sh
+++ HSMG Construction and Annotation/run.sh
@@ -7,7 +7,7 @@ binning outfile = $4
 metawrap assembly -1 $1 -2 $2  -o $3 -t 20 -m 2000
 
 metawrap --metabat2 --maxbin2 --concoct -t 20 -m 2000 --run-checkm -a $3/final_assembly.fasta -o $4 $1 $2
-
+DAS_TOOL -i metabat2_bin.tsv,maxbin2_bin.tsv,concoct_bin.tsv -l metabat,maxbin,concoct -c $3/final_assembly.fasta -o  DAS_out --search_engine diamond --write_bins 1
 
 drep install : https://github.com/MrOlm/drep
 dRep compare ./dRep_$4/ -p 20 -pa 0.95 -sa 0.95 -g ./$4/*.fa > $4_compare.log
