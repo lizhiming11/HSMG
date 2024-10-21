cb630ea83bacc27d7adf00c2a61f9a5881ddb7f9
lizm <642998881@qq.com>
2024-10-21 15:55:14 +0800

    Update run.sh

--- HSMG Construction and Annotation/run.sh
+++ HSMG Construction and Annotation/run.sh
@@ -1,25 +1,30 @@
-metaWRAP install : https://github.com/bxlab/metaWRAP
-fq1 = $1
-fq2 = $2
-assembly outfile = $3
+#metaWRAP install : https://github.com/bxlab/metaWRAP
+sample_name = $1
+
+#assembly
+metawrap assembly -1 $1.1.fq.gz -2 $1.2.fq.gz  -o $1 -t 20 -m 2000
+
+metawrap --metabat2 --maxbin2 --concoct -t 20 -m 2000 --run-checkm -a $1/final_assembly.fasta -o $1_binning $1.1.fq.gz $1.2.fq.gz
+#quality
+DAS_TOOL -i metabat2_bin.tsv,maxbin2_bin.tsv,concoct_bin.tsv -l metabat,maxbin,concoct -c $1/final_assembly.fasta -o  DAS_out --search_engine diamond --write_bins 1
+#High-quality (HQ) genomes (completeness >=90%, contamination <=5%), Medium-quality (MQ) genomes (completeness >=50%, contamination <=10%)
+checkm lineage_wf DAS_out/ checkm_ann -x fa -t 20
+
+#drep install : https://github.com/MrOlm/drep
+
+#Merge the medium-quality and above MAGs into one folder.
+#clustering
 binning outfile = $4
+dRep compare ./dRep/ -p 20 -pa 0.95 -sa 0.95 -g ./$4/*.fa > $4_compare.log
 
-metawrap assembly -1 $1 -2 $2  -o $3 -t 20 -m 2000
-
-metawrap --metabat2 --maxbin2 --concoct -t 20 -m 2000 --run-checkm -a $3/final_assembly.fasta -o $4 $1 $2
-DAS_TOOL -i metabat2_bin.tsv,maxbin2_bin.tsv,concoct_bin.tsv -l metabat,maxbin,concoct -c $3/final_assembly.fasta -o  DAS_out --search_engine diamond --write_bins 1
-
-drep install : https://github.com/MrOlm/drep
-dRep compare ./dRep_$4/ -p 20 -pa 0.95 -sa 0.95 -g ./$4/*.fa > $4_compare.log
-
-gtdbtk annotation：
+#Annotate the dereplicated representative genomes using GTDB (Genome Taxonomy Database).
+#gtdbtk annotation：
 gtdbtk identify --genome_dir genomics-dir --out_dir identify --extension fa --cpus 40
 gtdbtk align --identify_dir identify --out_dir align --cpus 40
 gtdbtk classify --genome_dir genomics-dir --align_dir align --out_dir classify -x fa --cpus 40 
 
-phylogenetic tree:
+#phylogenetic tree:
 iqtree2 -s gtdbtk.bac120.user_msa.fasta -m MFP
 
-
-Functional annotation:
+#Functional annotation:
 python emapper.py -m diamond -i genome_dir/*.fa --output result -d bact --cpu 5
